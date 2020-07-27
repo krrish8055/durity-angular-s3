97751bee946fface4e9af1a4b9e1a78065cbe70d
Ubuntu <ubuntu@ip-172-31-40-186.ap-south-1.compute.internal>
2020-07-27 10:10:07 +0000

    updated angular app

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { APIService } from '../API.service';
 import { User } from '../user';
 import { Auth } from 'aws-amplify';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -11,17 +13,65 @@ export class ProfileComponent implements OnInit {
   userId: string;
   userName: string;
   user = new User('', '', '', '', '', '');
+  showPhoto: boolean;
+  userCreated: boolean;
+
   constructor(private api: APIService) {}
   ngOnInit() {
     Auth.currentAuthenticatedUser({
-        bypassCache: false
-      }).then(async user => {
-        this.userId = user.attributes.sub;
-        this.userName = user.username;
+      bypassCache: false
+    }).then(async user => {
+       this.userName = user.username;
+       this.userId = user.attributes.sub;
+       let result = await this.api.GetUser(this.userId);
+       if (!result) {
+          this.userCreated = false;
+          this.user = new User('', '', '', '', '', '');
+       } else {
+         this.userCreated = true;
+         this.showPhoto = !!result.image;
+         this.user = new User(
+           this.userId,
+           result.username,
+           result.firstName,
+           result.lastName,
+           result.bio,
+           result.image
+          )
+        }
       })
       .catch(err => console.log(err));
   }
-  
+
+  editPhoto() {
+    this.showPhoto = false;
+  }
+
+ async onImageUploaded(e) {
+    this.user.imageUrl = e.key;
+    if (this.userCreated) {
+      await this.api.UpdateUser({
+        id: this.userId,
+        image: this.user.imageUrl
+      });
+    }
+    this.showPhoto = true;
+  }
+
+  logOut() {
+    Auth.signOut({ global: true })
+    .then(data => {
+      this.router.navigate(['/auth']);
+    })
+    .catch(err => console.log(err));
+  }
+
+
+  getType(): string {
+    return this.userCreated ? 'UpdateUser' : 'CreateUser';
+  }
+
+
   async updateProfile() {
     const user = {
       id: this.userId,
@@ -29,7 +79,8 @@ export class ProfileComponent implements OnInit {
       firstName: this.user.firstName,
       lastName: this.user.lastName,
       bio: this.user.aboutMe
+      image: this.user.imageUrl
     }
-    await this.api.CreateUser(user);
+    await this.api[this.getType()](user);
   }
 }
